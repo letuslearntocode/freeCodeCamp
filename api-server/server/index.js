722f4af228bb28de280e0629f5fb776b466c99d8
Oliver Eyton-Williams <ojeytonwilliams@gmail.com>
2020-03-23 12:59:41 +0100

    fix: initialize Sentry before boot
    Now boot errors will be reported

--- api-server/server/index.js
+++ api-server/server/index.js
@@ -7,7 +7,10 @@ const loopback = require('loopback');
 const boot = require('loopback-boot');
 const expressState = require('express-state');
 const createDebugger = require('debug');
+const Sentry = require('@sentry/node');
 
+const { isHandledError } = require('./utils/create-handled-error.js');
+const { sentry } = require('../../config/secrets');
 const { setupPassport } = require('./component-passport');
 
 const log = createDebugger('fcc:server');
@@ -15,6 +18,24 @@ const log = createDebugger('fcc:server');
 // force logger to always output
 // this may be brittle
 log.enabled = true;
+
+if (sentry.dns === 'dsn_from_sentry_dashboard') {
+  log('Sentry reporting disabled unless DSN is provided.');
+} else {
+  Sentry.init({
+    dsn: sentry.dns,
+    beforeSend(event, hint) {
+      log('REPORTING TO SENTRY', hint.originalException.message);
+      log('EVENT', event);
+      log('ishandled?', isHandledError(hint.originalException));
+      // DEBUG: returning null stops the event from being reported to Sentry
+      return null;
+      // DEBUG: use the next return to send to Sentry
+      // return isHandledError(hint.originalException) ? null : event;
+    }
+  });
+  log('Sentry initialized');
+}
 
 Rx.config.longStackSupport = process.env.NODE_DEBUG !== 'production';
 const app = loopback();
